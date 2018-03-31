763d70f7a90523f3f4aa0bc024e690a08d174764
Pawel Bogucki <pawelbogucki64@gmail.com>
2018-03-31 14:44:28 +0000

    Remove initial css

--- src/js/osa.js
+++ src/js/osa.js
@@ -1,22 +1,29 @@
-/* global ScrollMagic, TweenLite, Bounce */
 class Osa {
-    constructor(cssClass, speed = 1, greenSock = {x: 0, y: 0, autoAlpha: 1}) {
+    constructor(cssSelector, speed = 1, gsInitial = {y: 30, autoAlpha: 0}, gsFinal = {x: 0, y: 0, autoAlpha: 1}) {
         this.speed = speed;
-        this.cssClass = cssClass;
-        this.greenSock = greenSock;
+        this.cssSelector = cssSelector;
+        this.gsFinal = gsFinal;
+        this.gsInitial = gsInitial;
 
         var controller = new ScrollMagic.Controller();
 
         var osaObject = this; //reference to the current instance
 
-        $(this.cssClass).each(function () {
+        //set up initial positioning
+        TweenLite.to(cssSelector, 0, gsInitial);
+
+        $(cssSelector).each(function () {
             var domRef = this; //reference to a single dom element of the loop
-            new ScrollMagic.Scene({triggerElement: domRef})
-                    .triggerHook(0.9)
-                    .addIndicators()
+            var animated = false;
+            new ScrollMagic.Scene({triggerElement: domRef, duration: 100})
+                    .triggerHook(0.5)
+                    //.addIndicators()
                     .addTo(controller)
-                    .on('enter leave', function () {
-                        TweenLite.to(domRef, osaObject.speed, osaObject.greenSock);
+                    .on('progress', function (e) {
+                        if (e.state == 'DURING' && !animated) {
+                            animated = true;
+                            TweenLite.to(domRef, osaObject.speed, osaObject.gsFinal);
+                        }
                     });
         });
     }
